634faaef634b93865a1bd4db6fe9cd1e234deb49
Goran Maslic <goranmaslic@outlook.com>
2017-06-07 20:18:53 +0200

    Initial project push

---
+++ src/client/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NameListService } from '../shared/name-list/name-list.service';
+
+/**
+ * This class represents the lazy loaded HomeComponent.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'sd-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css'],
+})
+export class HomeComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+   
+  }
+}
