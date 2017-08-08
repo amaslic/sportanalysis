dded60e830109a0463793fb232f0859ece2b7582
Brijesh Vora <brijesh.vora@plutustec.com>
2017-08-08 17:57:42 +0530

    Serch Input box feature enabled

--- src/app/pipes/searchEvent.pipe.ts
+++ src/app/pipes/searchEvent.pipe.ts
@@ -11,34 +11,85 @@ export class SearchEventPipe implements PipeTransform {
     public transform(value, keys: string, term: any) {
         let team = term[0];
         let event = term[1];
+        let search = term[2];
+        console.log(search);
 
-        if ((!team || team.length == 0) && (!event || event.length == 0)) return value;
-        return (value || []).filter((item) => {
-            //keys.split(',').some(key => item.hasOwnProperty(key) && term.indexOf(item[key]) > -1)
-            let result = false;
-            if (team && team.length > 0) {
-                team.forEach(t => {
-                    if (t == item['team']) {
-                        result = true;
-                        if (event && event.length > 0) {
-                            result = false;
-                            event.forEach(e => {
-                                if (e == item['name'])
-                                    result = true;
-                            });
+        if (!search || search.length == 0) {
+            if ((!team || team.length == 0) && (!event || event.length == 0)) return value;
+            return (value || []).filter((item) => {
+                //keys.split(',').some(key => item.hasOwnProperty(key) && term.indexOf(item[key]) > -1)
+                let result = false;
+                if (team && team.length > 0) {
+                    team.forEach(t => {
+                        if (t == item['team']) {
+                            result = true;
+                            if (event && event.length > 0) {
+                                result = false;
+                                event.forEach(e => {
+                                    if (e == item['name'])
+                                        result = true;
+                                });
+                            }
                         }
-                    }
-                });
-            }
-            if ((!team || team.length == 0) && event && event.length > 0) {
-                result = false;
-                event.forEach(e => {
-                    if (e == item['name'])
+                    });
+                }
+                if ((!team || team.length == 0) && event && event.length > 0) {
+                    result = false;
+                    event.forEach(e => {
+                        if (e == item['name'])
+                            result = true;
+                    });
+                }
+                return result;
+            });
+        }
+        else {
+            // if (!search) return value;
+            // return (value || []).filter((item) => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(search, 'gi').test(item[key])));
+
+            return (value || []).filter((item) => {
+                //keys.split(',').some(key => item.hasOwnProperty(key) && term.indexOf(item[key]) > -1)
+                let result = false;
+
+
+
+                if ((!search || search.length > 0)) {
+                    console.info('Name ' + item['name']);
+                    console.info('team ' + item['team']);
+                    console.info('search ' + search);
+
+                    if ((item['name'] && item['name'].toLowerCase().indexOf(search.toLowerCase()) != -1) || (item['team'] && item['team'].toLowerCase().indexOf(search.toLowerCase()) != -1)) {
                         result = true;
-                });
-            }
-            return result;
-        });
+                    }
+                    if ((!team || team.length == 0) && event && event.length > 0) {
+                        result = false;
+                        event.forEach(e => {
+                            if (e == item['name'])
+                                result = true;
+                        });
+                    }
+                    if (team && team.length > 0) {
+                        team.forEach(t => {
+                            if (t == item['team']) {
+                                result = true;
+                                if (event && event.length > 0) {
+                                    result = false;
+                                    event.forEach(e => {
+                                        if (e == item['name'])
+                                            result = true;
+                                    });
+                                }
+                            }
+                        });
+                    }
+                }
+
+
+                return result;
+
+            });
+        }
+
     }
 
     // transform(items: Array<any>, filter: { [key: string]: any }): Array<any> {
