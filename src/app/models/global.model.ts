02d9336c52ff321582227048fdfc7ef6b1cc7b5d
Brijesh Vora <brijesh.vora@plutustec.com>
2017-08-11 14:44:37 +0530

    revert angular2 modal

--- src/app/models/global.model.ts
+++ src/app/models/global.model.ts
@@ -1,7 +1,7 @@
 export const GlobalVariables = Object.freeze({
-     BASE_API_URL: 'http://136.144.141.55:3000/api/',
+    BASE_API_URL: 'http://136.144.141.55:3000/api/',
     // BASE_API_URL: 'http://localhost:3000/api/',
-     BASE_VIDEO_URL: 'http://136.144.141.55:3000',
-     BASE_IMAGE_URL: 'http://136.144.141.55:3000',
-     BASE_TRACKINGDATA_URL: 'http://136.144.141.55:3000'
- });
+    BASE_VIDEO_URL: 'http://136.144.141.55:3000',
+    BASE_IMAGE_URL: 'http://136.144.141.55:3000',
+    BASE_TRACKINGDATA_URL: 'http://136.144.141.55:3000'
+});
