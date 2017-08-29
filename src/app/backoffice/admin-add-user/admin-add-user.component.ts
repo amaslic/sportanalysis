0c1fd1f58187e6d40735d48ae4fd2a6f6b604131
shashank <shashank.shah@plutustec.com>
2017-08-29 20:46:52 +0530

    added new user which can be added by admin.

---
+++ src/app/backoffice/admin-add-user/admin-add-user.component.ts
@@ -0,0 +1,112 @@
+import {
+  Component,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import {
+  UserService
+} from './../../services/user.service';
+import {
+  User
+} from './../../models/user.model';
+import {
+  Router
+} from '@angular/router';
+import {
+  ClubService
+} from './../../services/club.service';
+import { FormControl } from "@angular/forms";
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+import { CompleterService, CompleterData } from 'ng2-completer';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './admin-add-user.component.html',
+  styleUrls: ['./admin-add-user.css']
+})
+export class AdminAddUserComponent implements OnInit {
+  public user: User = new User();
+  successmsg: string;
+  errormsg: string;
+
+  private router: Router;
+  clubCtrl: FormControl;
+  filteredClubs: any;
+  clubData = ['test'];
+  activatedClubList: any;
+
+
+  @ViewChild('regSucessModal') regSucessModal;
+  @ViewChild('regErrorModal') regErrorModal;
+  constructor(private completerService: CompleterService, private clubService: ClubService, private userService: UserService, r: Router) {
+    this.router = r;
+    this.clubCtrl = new FormControl();
+
+    // this.dataService = completerService.local(this.searchData, 'color', 'color');
+  }
+
+  ngOnInit() {
+    this.getActivatedClubs();
+    this.filteredClubs = this.clubCtrl.valueChanges
+      .startWith(null)
+      .map(name => this.filterClubs(name));
+
+  }
+  filterClubs(val: string) {
+    return val ? this.clubData.filter(s => s.toLowerCase().indexOf(val.toLowerCase()) === 0)
+      : this.clubData;
+  }
+
+
+  onSubmit(f) {
+    if (!f.valid) {
+      return false;
+    }
+    console.log(this.user);
+    this.userService.createNewUser(this.user)
+      .subscribe(
+      (response) => this.onSignupSuccess(response),
+      (error) => this.onError(error)
+      );
+  }
+  regSuccessPopupClose() {
+    this.router.navigateByUrl('/backoffice/users');
+  }
+  onSignupSuccess(response) {
+    const responseBody = JSON.parse(response._body);
+    console.log(responseBody);
+    // alert(responseBody.msg);
+    this.successmsg = responseBody.message;
+    this.regSucessModal.open();
+
+    this.user = new User();
+    // this.router.navigateByUrl('/auth/login');
+  }
+
+  onError(error) {
+    const errorBody = JSON.parse(error._body);
+    console.error(errorBody);
+    this.errormsg = errorBody.message;
+    this.regErrorModal.open();
+
+    // alert(errorBody.msg);
+  }
+
+  getActivatedClubs() {
+    this.clubService.getActivatedClubs(this.userService.token).subscribe(
+      (response) => this.onGetActivatedClubsSuccess(response),
+      (error) => this.onError(error)
+    );
+  }
+
+  onGetActivatedClubsSuccess(response) {
+    this.activatedClubList = JSON.parse(response._body);
+    this.activatedClubList.forEach(element => {
+      this.clubData.push(element.name);
+    });
+
+  }
+}
+
