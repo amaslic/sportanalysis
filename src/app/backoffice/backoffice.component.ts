93c4e6a281d48c4deed9a17fcc2ca7bba4f26032
Brijesh Vora <brijesh.vora@plutustec.com>
2017-09-11 20:19:18 +0530

    Backoffice Logout implemented

--- src/app/backoffice/backoffice.component.ts
+++ src/app/backoffice/backoffice.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  LocalStorageService
+} from 'angular-2-local-storage';
+import {
+  Router, ActivatedRoute
+} from '@angular/router';
 
 @Component({
   selector: 'app-backoffice',
@@ -9,14 +15,19 @@ export class BackofficeComponent implements OnInit {
 
 
   private _opened = true;
-
+  private router: Router;
   private _toggleSidebar() {
     this._opened = !this._opened;
   }
 
-  constructor() { }
+  constructor(private localStorageService: LocalStorageService, r: Router) { this.router = r; }
 
   ngOnInit() {
+  }
+  logout() {
+    this.localStorageService.remove('token');
+    this.localStorageService.remove('user');
+    this.router.navigateByUrl('/auth/login');
   }
 
 }
