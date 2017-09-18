bb3538c79e615426941e7da51589eedcffee2a3c
Brijesh Vora <brijesh.vora@plutustec.com>
2017-09-18 19:14:04 +0530

    Video setting page changes

--- src/app/main/videos/settings/settings.component.ts
+++ src/app/main/videos/settings/settings.component.ts
@@ -201,130 +201,134 @@ export class VideoSettingsComponent implements OnInit {
     }
 
     this.showProgressBar = true;
+    if (f.value.type != 'Training') {
+      var clubName = f.value.clubName;
+      if (clubName != '' && clubName != null) {
+        var ClubData1 = this.allClubList.filter(function (element, index) {
+          return (element.name.toLowerCase() === clubName.toLowerCase());
+        })[0];
+      }
 
-    var clubName = f.value.clubName;
-    if (clubName != '' && clubName != null) {
-      var ClubData1 = this.allClubList.filter(function (element, index) {
-        return (element.name.toLowerCase() === clubName.toLowerCase());
-      })[0];
-    }
+      var clubName2 = f.value.clubName2;
+      if (clubName2 != '' && clubName2 != null) {
+        var ClubData2 = this.allClubList.filter(function (element, index) {
+          return (element.name.toLowerCase() === clubName2.toLowerCase());
+        })[0];
+      }
 
-    var clubName2 = f.value.clubName2;
-    if (clubName2 != '' && clubName2 != null) {
-      var ClubData2 = this.allClubList.filter(function (element, index) {
-        return (element.name.toLowerCase() === clubName2.toLowerCase());
-      })[0];
-    }
+      var teamName1 = f.value.team1;
+      if (teamName1 != '' && teamName1 != null) {
+        var TeamData1 = this.allClubList.filter(function (element, index) {
+          return (element.name.toLowerCase() === teamName1.toLowerCase());
+        })[0];
+      }
 
-    var teamName1 = f.value.team1;
-    if (teamName1 != '' && teamName1 != null) {
-      var TeamData1 = this.allClubList.filter(function (element, index) {
-        return (element.name.toLowerCase() === teamName1.toLowerCase());
-      })[0];
-    }
+      var teamName2 = f.value.team2;
+      if (teamName2 != '' && teamName2 != null) {
+        var TeamData2 = this.allClubList.filter(function (element, index) {
+          return (element.name.toLowerCase() === teamName2.toLowerCase());
+        })[0];
+      }
 
-    var teamName2 = f.value.team2;
-    if (teamName2 != '' && teamName2 != null) {
-      var TeamData2 = this.allClubList.filter(function (element, index) {
-        return (element.name.toLowerCase() === teamName2.toLowerCase());
-      })[0];
-    }
+      this.notExistedClub = [];
+      if (typeof (ClubData1) == 'undefined' && clubName && clubName != '' && clubName != null) {
+        this.notExistedClub.push({ name: clubName });
+      } else {
+        f.value.clubName = ClubData1._id;
+      }
 
-    this.notExistedClub = [];
-    if (typeof (ClubData1) == 'undefined' && clubName && clubName != '' && clubName != null) {
-      this.notExistedClub.push({ name: clubName });
-    } else {
-      f.value.clubName = ClubData1._id;
-    }
+      if (typeof (ClubData2) == 'undefined' && clubName2 && clubName2 != '' && clubName2 != null) {
 
-    if (typeof (ClubData2) == 'undefined' && clubName2 && clubName2 != '' && clubName2 != null) {
+        var existclub = this.notExistedClub.filter(function (element, index) {
+          return (element.name.toLowerCase() === clubName2.toLowerCase());
+        });
 
-      var existclub = this.notExistedClub.filter(function (element, index) {
-        return (element.name.toLowerCase() === clubName2.toLowerCase());
-      });
+        if (typeof (existclub) == 'undefined' || existclub.length == 0)
+          this.notExistedClub.push({ name: clubName2 });
+      } else {
+        f.value.clubName2 = ClubData2._id;
+      }
 
-      if (typeof (existclub) == 'undefined' || existclub.length == 0)
-        this.notExistedClub.push({ name: clubName2 });
-    } else {
-      f.value.clubName2 = ClubData2._id;
-    }
+      if (typeof (TeamData1) == 'undefined' && teamName1 && teamName1 != '' && teamName1 != null) {
+        var existclub = this.notExistedClub.filter(function (element, index) {
+          return (element.name.toLowerCase() === teamName1.toLowerCase());
+        });
 
-    if (typeof (TeamData1) == 'undefined' && teamName1 && teamName1 != '' && teamName1 != null) {
-      var existclub = this.notExistedClub.filter(function (element, index) {
-        return (element.name.toLowerCase() === teamName1.toLowerCase());
-      });
+        if (typeof (existclub) == 'undefined' || existclub.length == 0)
+          this.notExistedClub.push({ name: teamName1 });
+      } else {
+        if (teamName1 && teamName1 != '' && teamName1 != null)
+          f.value.team1 = TeamData1._id;
+        else
+          f.value.team1 = '';
+      }
 
-      if (typeof (existclub) == 'undefined' || existclub.length == 0)
-        this.notExistedClub.push({ name: teamName1 });
-    } else {
-      if (teamName1 && teamName1 != '' && teamName1 != null)
-        f.value.team1 = TeamData1._id;
-      else
-        f.value.team1 = '';
-    }
+      if (typeof (TeamData2) == 'undefined' && teamName2 && teamName2 != '' && teamName2 != null) {
+        var existclub = this.notExistedClub.filter(function (element, index) {
+          return (element.name.toLowerCase() === teamName2.toLowerCase());
+        });
 
-    if (typeof (TeamData2) == 'undefined' && teamName2 && teamName2 != '' && teamName2 != null) {
-      var existclub = this.notExistedClub.filter(function (element, index) {
-        return (element.name.toLowerCase() === teamName2.toLowerCase());
-      });
+        if (typeof (existclub) == 'undefined' || existclub.length == 0)
+          this.notExistedClub.push({ name: teamName2 });
+      } else {
+        if (teamName2 && teamName2 != '' && teamName2 != null)
+          f.value.team2 = TeamData2._id;
+        else
+          f.value.team2 = '';
+      }
 
-      if (typeof (existclub) == 'undefined' || existclub.length == 0)
-        this.notExistedClub.push({ name: teamName2 });
-    } else {
-      if (teamName2 && teamName2 != '' && teamName2 != null)
-        f.value.team2 = TeamData2._id;
-      else
-        f.value.team2 = '';
-    }
+      if (this.notExistedClub.length > 0) {
+        var count = 0;
+        this.notExistedClub.forEach(element => {
+          this.clubService.createClub({ name: element.name })
+            .subscribe(
+            (response1: any) => {
+              count++;
 
-    if (this.notExistedClub.length > 0) {
-      var count = 0;
-      this.notExistedClub.forEach(element => {
-        this.clubService.createClub({ name: element.name })
-          .subscribe(
-          (response1: any) => {
-            count++;
+              var response = JSON.parse(response1._body);
+              element.clubId = response.club._id;
+              this.allClubList.push(response.club);
 
-            var response = JSON.parse(response1._body);
-            element.clubId = response.club._id;
-            this.allClubList.push(response.club);
+              if (this.notExistedClub.length == count) {
 
-            if (this.notExistedClub.length == count) {
+                if (typeof (ClubData1) == 'undefined' && clubName && clubName != '' && clubName != null) {
+                  var existclub = this.notExistedClub.filter(function (element, index) {
+                    return (element.name.toLowerCase() === clubName.toLowerCase());
+                  })[0];
+                  f.value.clubName = existclub.clubId;
+                }
 
-              if (typeof (ClubData1) == 'undefined' && clubName && clubName != '' && clubName != null) {
-                var existclub = this.notExistedClub.filter(function (element, index) {
-                  return (element.name.toLowerCase() === clubName.toLowerCase());
-                })[0];
-                f.value.clubName = existclub.clubId;
+                if (typeof (ClubData2) == 'undefined' && clubName2 && clubName2 != '' && clubName2 != null) {
+                  var existclub = this.notExistedClub.filter(function (element, index) {
+                    return (element.name.toLowerCase() === clubName2.toLowerCase());
+                  })[0];
+                  f.value.clubName2 = existclub.clubId;
+                }
+
+                if (typeof (TeamData1) == 'undefined' && teamName1 && teamName1 != '' && teamName1 != null) {
+                  var existclub = this.notExistedClub.filter(function (element, index) {
+                    return (element.name.toLowerCase() === teamName1.toLowerCase());
+                  })[0];
+                  f.value.team1 = existclub.clubId;
+                }
+
+                if (typeof (TeamData2) == 'undefined' && teamName2 && teamName2 != '' && teamName2 != null) {
+                  var existclub = this.notExistedClub.filter(function (element, index) {
+                    return (element.name.toLowerCase() === teamName2.toLowerCase());
+                  })[0];
+                  f.value.team2 = existclub.clubId;
+                }
+                this.updateVideo(f);
               }
+            },
+            (error) => this.onError(error)
+            );
 
-              if (typeof (ClubData2) == 'undefined' && clubName2 && clubName2 != '' && clubName2 != null) {
-                var existclub = this.notExistedClub.filter(function (element, index) {
-                  return (element.name.toLowerCase() === clubName2.toLowerCase());
-                })[0];
-                f.value.clubName2 = existclub.clubId;
-              }
-
-              if (typeof (TeamData1) == 'undefined' && teamName1 && teamName1 != '' && teamName1 != null) {
-                var existclub = this.notExistedClub.filter(function (element, index) {
-                  return (element.name.toLowerCase() === teamName1.toLowerCase());
-                })[0];
-                f.value.team1 = existclub.clubId;
-              }
-
-              if (typeof (TeamData2) == 'undefined' && teamName2 && teamName2 != '' && teamName2 != null) {
-                var existclub = this.notExistedClub.filter(function (element, index) {
-                  return (element.name.toLowerCase() === teamName2.toLowerCase());
-                })[0];
-                f.value.team2 = existclub.clubId;
-              }
-              this.updateVideo(f);
-            }
-          },
-          (error) => this.onError(error)
-          );
-
-      });
+        });
+      }
+      else {
+        this.updateVideo(f);
+      }
     } else {
       this.updateVideo(f);
     }
