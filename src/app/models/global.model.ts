3b149f29678418cf5f0208b742097533eff0fca0
Gene Karasev <phptechs@yahoo.com>
2017-09-20 14:34:39 +0300

    updated base video url for s3

--- src/app/models/global.model.ts
+++ src/app/models/global.model.ts
@@ -1,7 +1,7 @@
 export const GlobalVariables = Object.freeze({
     //Live server links
     BASE_API_URL: 'http://136.144.141.55:3000/api/',
-    BASE_VIDEO_URL: 'http://136.144.141.55:3000',
+    BASE_VIDEO_URL: 'https://s3.eu-central-1.amazonaws.com/sportsanalysis',
     BASE_IMAGE_URL: 'http://136.144.141.55:3000',
     BASE_TRACKINGDATA_URL: 'http://136.144.141.55:3000',
     BASE_URL: 'http://136.144.141.55/'
